daf0b3f9e58f940616a0982c9592ee6eb7cbfc93
Shrik2302 <53657697+Shrik2302@users.noreply.github.com>
2023-11-26 16:21:44 +0530

    Create create_database.py
    to create database_inside MySQL DB

---
+++ MySQL_with_Python/create_database.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Root@123",
+    )
+
+cursor=mydb.cursor()
+
+#creating database
+#cursor.execute("CREATE DATABASE employee")
+
+#selecting table
+cursor.execute("USE employee")
+# Creating table
+table="""CREATE TABLE employee_data (
+        emp_no INT(11) NOT NULL AUTO_INCREMENT,
+        birth_date DATE NOT NULL,
+        first_name VARCHAR(14) NOT NULL,
+        last_name  VARCHAR(20) NOT NULL,
+        PRIMARY KEY (emp_no)) """
+cursor.execute(table)
+mydb.close()
